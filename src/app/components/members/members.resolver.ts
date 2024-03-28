70de64c58c8fb5bb22ff5618a1666789546f024d
cliffeby <cliffeby@gmail.com>
2024-03-27 23:56:19 -0400

    v16_17

--- src/app/components/members/members.resolver.ts
+++ src/app/components/members/members.resolver.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { Router, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { EMPTY, Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Member } from 'src/app/models/member';
@@ -12,7 +8,7 @@ import { MembersService } from 'src/app/services/members.service';
 @Injectable({
   providedIn: 'root'
 })
-export class MembersResolver implements Resolve<Member[]> {
+export class MembersResolver  {
   constructor(private _membersService: MembersService, private router : Router) { }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Member[]> {
     return this._membersService.getMembers().pipe(
